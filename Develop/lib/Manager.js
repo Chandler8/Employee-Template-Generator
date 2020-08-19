cb78c41d03bbd780477e19381fe0ea473ec76b06
Chandler L <cbl0015@auburn.edu>
2020-08-19 00:29:23 -0400

    Created Employee module and other corresponding modules

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,21 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+// Manager class that utilizes employee module, it uniquely asks for managers office number
+
+const Employee = require("./Employee");
+
+class Manager extends Employee {
+    constructor(name, id, email, officeNo){
+
+        super(name, id, email);
+
+        this.officeNumber = officeNo;
+        
+    }
+    getOfficeNumber(){
+        return this.officeNumber;
+    }
+    getRole(){
+        return "Manager";
+    }
+}
+
+module.exports = Manager;
